f235a95e7c26f188344dbcdc0e9fd161e5aa2ad2
d_barnett <domoniquebarnett7@gmail.com>
2017-03-19 00:54:35 +0000

    error handling

--- myCtrl.js
+++ myCtrl.js
@@ -1,9 +1,17 @@
 app.controller("myCtrl", function($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
     $scope.addItem = function () {
-        $scope.products.push($scope.addMe);
+        $scope.errortext = "";
+        if (!$scope.addMe) {return;}
+        if ($scope.products.indexOf($scope.addMe) == -1) {
+            $scope.products.push($scope.addMe);
+        } else {
+            $scope.errortext = "The item is already in your shopping list.";
+        }
+        
     } 
     $scope.removeItem = function (x) {
+        $scope.errortext = "";
         $scope.products.splice(x, 1);
     } 
 });
